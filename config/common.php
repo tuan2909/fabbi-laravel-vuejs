27685080caaf1cb8899967df82920a376f2e7a46
NQT Chido <nqtsuper2909@gmail.com>
2021-06-08 17:00:20 +0700

    perf: Delete common file

--- config/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'lang' => [
-        'en' => [
-            'message_api_true' => 'Loading Api Success',
-            'message_api_false' => 'Loading Api False'
-        ]
-    ]
-];
