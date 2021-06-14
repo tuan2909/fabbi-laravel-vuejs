e364c110695845827e33f82bdccf9d7070d776c0
NQT Chido <nqtsuper2909@gmail.com>
2021-06-14 22:40:34 +0700

    feat: Master layout

---
+++ fabbi-user/src/layout/Wrappers/UserLayout/baseLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <h1>User Layout</h1>
+    <slot/>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- fabbi-user/src/main.js
+++ fabbi-user/src/main.js
@@ -4,7 +4,16 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+//Admin layout
+import AdminLayout from './layout/Wrappers/AdminLayout/baseLayout'
+// User layout
+import UserLayout from './layout/Wrappers/UserLayout/baseLayout'
+// Default layout
+import DefaultLayout from './layout/Wrappers/defaultLayout'
 
+Vue.component('user-layout', UserLayout)
+Vue.component('default-layout', DefaultLayout)
+Vue.component('admin-layout', AdminLayout)
 new Vue({
     router,
     store,
